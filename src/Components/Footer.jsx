3bb102d58f2b2e8054ef12cc84ab8ce9c78ae14a
MilBe2007 <mileny.santos@estudante.ifms.edu.br>
2024-03-22 08:43:50 -0400

    Atividade 28_02

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+export default function Footer(){
+
+    return(
+    <footer>
+        <h1>Desenvolvido por: Mileny Beatriz Gonçalves dos Santos.</h1>
+    </footer>
+        
+        
+    );
+        
+}
+        

815e744a68d207c653a295a8905123bd914990c5
MilBe2007 <mileny.santos@estudante.ifms.edu.br>
2024-03-27 16:06:51 -0400

    27-03

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -4,20 +4,34 @@ export default function Main(){
 
     const [nome, setNome] = useState("");
     const [telefone, setTel] = useState("");
+    const [listaContatos, setContatos] = useState( [ ] );
+
+    const registrar = (event) => {
+        event.preventDefault();
+        alert("Deu certo!");
+        sertContatos([...listaContatos,
+        {
+            nomeSalvo: nome,
+            telefone: telefone
+        }
+    ])
+    }
+    console.table(listaContatos);
 
     return(
         <main>
-            <form>
-                <label htmlForm="Nome">
-                    Nome:
+            <form onSubmit={registrar}>
+                <label htmlFor="Nome">
+                    Nome:  
                 </label>
             <input 
             type="text" 
             name="" 
             id="Nome" 
             onChange={(event)=> setNome(event.target.value)}/>
-                <label htmlForm="telefone">
-                    Telefone:
+
+                <label htmlFor="telefone">
+                    Telefone:  
                 </label>  
                 <input
                 type="tel"
@@ -29,7 +43,6 @@ export default function Main(){
                     Salvar
                 </button>
             </form>
-            
 
             {nome}
             {telefone}
